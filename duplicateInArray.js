94fcd01f1dc4f7254e3828a40a69c53203a64278
himanshuReact <74451686+himanshuReact@users.noreply.github.com>
2022-01-14 13:48:45 +0530

    Update duplicateInArray.js

--- duplicateInArray.js
+++ duplicateInArray.js
@@ -4,6 +4,15 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+
+// Solution 1
+
+var containsDuplicate = function(nums) {
+    let numSet = [...new Set(nums)]
+    return (nums.length>numSet.length?true: false)
+}
+
+// solution 2
 var containsDuplicate = function(nums) {
     nums.sort()
     for (let i = 0 ; i < nums.length;i++){
