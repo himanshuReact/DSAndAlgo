426b225035aebea549662a650cff69fc3c424f8c
himanshuReact <74451686+himanshuReact@users.noreply.github.com>
2022-01-14 13:19:14 +0530

    duplicateInArray.js

---
+++ duplicateInArray.js
@@ -0,0 +1,15 @@
+//  https://leetcode.com/problems/contains-duplicate/submissions/
+//  one of best optimised
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    nums.sort()
+    for (let i = 0 ; i < nums.length;i++){
+        if (nums[i]== nums[i+1]){
+            return true
+        }
+    }
+    return false
+};
