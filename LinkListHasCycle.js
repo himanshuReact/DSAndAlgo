06fea30d232dfab79db83847ddc4a4fdfd9cae1f
Himanshu Shekhar <74451686+himanshuReact@users.noreply.github.com>
2022-01-19 23:51:23 +0530

    LinkListHasCycle.js

---
+++ LinkListHasCycle.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if(!head || !head.next){
+        return false
+    }
+    let fast = head;
+    let slow = head;
+    
+    while( fast != null && fast.next !=null){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(fast == slow){
+           return true 
+        }
+       
+    }
+    return false;
+    
+};
