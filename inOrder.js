b7e7c14a4ff7a09eb244017b12e111ca80a0636b
Himanshu Shekhar <74451686+himanshuReact@users.noreply.github.com>
2022-01-22 23:22:41 +0530

    inOrder.js

---
+++ inOrder.js
@@ -0,0 +1,51 @@
+// var inorderTraversal = function(root) {
+//     var result = [];
+    
+//     const inorder = (root) => {
+//         if(root === null){
+//             return;
+//         }
+        
+//         inorder(root.left);
+//         result.push(root.val);
+//         inorder(root.right);
+//     }
+    
+//     inorder(root);
+    
+//     return result;
+// };
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+let arr;
+var inorderTraversal = function(root) {
+   
+    arr =[]
+    arr =inorder(root)
+
+    return arr   
+};
+
+
+const inorder =(root)=>{
+    if(root!==null){
+        
+        inorder(root.left)   
+        console.log("hello",root.val )
+        arr.push(root.val)
+        inorder(root.right)
+    }
+    return arr 
+}
