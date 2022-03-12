34f22d0871c2735d2337a8613b2418c92ea3ce6b
Himanshu Shekhar <74451686+himanshuReact@users.noreply.github.com>
2022-03-13 01:00:10 +0530

    Update convertArr.js

--- convertArr.js
+++ convertArr.js
@@ -21,3 +21,30 @@ return large
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 let size = 3
 convertArr(arr,size)
+
+
+//alternate prototype 
+
+Array.prototype.chop = function(size){
+    //temp array
+    const temp = [...this];
+    
+    //if size is not defined
+    if(!size){
+      return temp;
+    }  
+  
+    //output
+    const output = [];
+    let i = 0;
+    
+    //iterate the array
+    while (i < temp.length) {
+        //slice the sub-array of given size
+        //and push them in output array
+        output.push(temp.slice(i, i + size));
+        i = i + size;
+    }
+  
+    return output;
+}
