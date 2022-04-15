2a0e26d495d41f0260116e743b77d4cd19a72535
Himanshu Shekhar <74451686+himanshuReact@users.noreply.github.com>
2022-04-15 15:56:50 +0530

    Update FIndPeak.js

--- FIndPeak.js
+++ FIndPeak.js
@@ -1,23 +1,50 @@
-//Find peak brute force approach 
-//https://leetcode.com/problems/find-peak-element/
+// //Find peak brute force approach 
+// //https://leetcode.com/problems/find-peak-element/
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var findPeakElement = function(nums) {
+    
+//     let arr = nums;
+    
+//     if(arr.length ==1) return 0;
+    
+//     for(let i =0; i< arr.length; i++){
+//         if((i ==0  && arr[i]> arr[i+1])|| (i==arr.length -1 && arr[i]> arr[i-1])){
+           
+//             return  i
+//         }
+//         else if((arr[i]> arr[i+1]   && arr[i]> arr[i-1])){
+//             return i
+//         }
+//     }
+//     return -1 
+// };
+
+
+//best optimise solution
+
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var findPeakElement = function(nums) {
+    let low = 0; 
+    let high = nums.length - 1;
     
-    let arr = nums;
-    
-    if(arr.length ==1) return 0;
-    
-    for(let i =0; i< arr.length; i++){
-        if((i ==0  && arr[i]> arr[i+1])|| (i==arr.length -1 && arr[i]> arr[i-1])){
-           
-            return  i
-        }
-        else if((arr[i]> arr[i+1]   && arr[i]> arr[i-1])){
-            return i
+    while (low < high) {
+        console.log({low})
+        console.log({high})
+        let mid = Math.floor(low + (high - low) / 2);
+        console.log({mid})
+        
+        if (nums[mid] > nums[mid + 1]) {
+            high = mid
+        } else {
+            low = mid + 1;
         }
     }
-    return -1 
+    
+    return low;
 };
