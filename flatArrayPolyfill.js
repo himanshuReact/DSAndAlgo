e0b2adee5b9d22359f31240f9077c2af879adc20
Himanshu Shekhar <74451686+himanshuReact@users.noreply.github.com>
2022-04-17 09:18:27 +0530

    Update flatArrayPolyfill.js

--- flatArrayPolyfill.js
+++ flatArrayPolyfill.js
@@ -1,5 +1,6 @@
 
 // https://learnersbucket.com/examples/array/how-to-flat-an-array-in-javascript/
+// infinite depth by default
 let arr = [[[1, [1.1]], 2, 3], [4, 5]]
 const flatArr = (arr,result=[])=>{
     for(let i=0;i< arr.length;i++){
@@ -14,3 +15,21 @@ const flatArr = (arr,result=[])=>{
 
 console.log(flatArr(arr))
 //[1, 1.1, 2, 3, 4, 5]
+
+
+/// based on depth
+let arr = [1,2,3,[3,4],[10,[7,8,[345]]]]
+function flatten(arr, depth=1){
+    let result = []
+    arr.map((ar)=>{
+        if(Array.isArray(ar) && depth>0){
+            depth = depth -1;
+            result.push(...flatten(ar,depth))
+        } else{
+            result.push(ar)
+        }        
+    })
+    return result;
+}
+flatten(arr,4)
+console.log(arr)
