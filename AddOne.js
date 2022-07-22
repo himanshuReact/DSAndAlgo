def6246045bbcfe95d7e9a319f6ad1142cf31283
Himanshu Shekhar <74451686+himanshuReact@users.noreply.github.com>
2022-07-22 22:51:43 +0530

    Create AddOne.js

---
+++ AddOne.js
@@ -0,0 +1,20 @@
+
+// https://leetcode.com/problems/plus-one/
+
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+
+var plusOne = function(digits) {
+for(var i = digits.length - 1; i >= 0; i--){
+     digits[i]++; 
+    if(digits[i] > 9){
+        digits[i] = 0;
+    }else{
+        return digits;
+    }
+}
+digits.unshift(1);
+return digits;
+};
